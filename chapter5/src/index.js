4abbab5bfc4ab08044045e1c0116c8ab33ce4301
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-10-28 10:02:42 +0700

    add updated port

--- chapter5/src/index.js
+++ chapter5/src/index.js
@@ -1,42 +1,42 @@
 //src/utils/index.js
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express"); //import express
-require("express-async-errors");
-const router = require("./routes");
-const fileUpload = require("express-fileupload");
-const { errorHandler, notFoundURLHandler } = require("./middlewares/errors");
+const express = require('express'); //import express
+require('express-async-errors');
+const router = require('./routes');
+const fileUpload = require('express-fileupload');
+const { errorHandler, notFoundURLHandler } = require('./middlewares/errors');
 
 const app = express(); //create express app
-const port = 3000; //set port
+const port = process.env.PORT || 4000;
 
 // Add middleware to parse JSON request bodies
 app.use(express.json());
 
 //we need to read form-body (body parser/reader) (req.files) if you want upload file
 app.use(
-  fileUpload({
-    limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB
-  })
+	fileUpload({
+		limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB
+	})
 );
 
 //create route for students page
-app.use("/", router);
+app.use('/', router);
 
 //create route for ping
 app.get('/', (req, res) => {
-  res.status(200).json({
-      message: "Ping Success!"
-  });
+	res.status(200).json({
+		message: 'Ping Success!',
+	});
 });
 
 // This function is for 404 handle URL
-app.use("*", notFoundURLHandler);
+app.use('*', notFoundURLHandler);
 
 // This function is to handle error when API hit, it always be the last middleware
 app.use(errorHandler);
 
 // Run the express server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
